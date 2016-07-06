f98384e93149270dc9d67ac22421a437f901d5e1
hfcc8685 <hfcc8685@gmail.com>
2016-07-06 13:40:29 +0800

    [hanfeng] first react page

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,1 +1,10 @@
-document.writeln("hello world! test webpack, test webpack watch");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+    render() {
+        return <h1>hello world! oh my God!</h1>
+    }
+}
+
+ReactDOM.render(<Hello/>, document.getElementById('content'));
--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     output: {
         path: './webapp/bin',
         filename: 'index.bundle.js'
+    },
+    module:{
+        loaders: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
     }
 };
